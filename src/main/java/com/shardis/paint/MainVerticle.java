56943528918b8ac340689aa7cc28756f8c447d0c
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2017-08-20 18:28:04 +0200

    added messages per minute counter

--- src/main/java/com/shardis/paint/MainVerticle.java
+++ src/main/java/com/shardis/paint/MainVerticle.java
@@ -7,6 +7,8 @@ import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class MainVerticle extends AbstractVerticle {
 
     @Override
@@ -39,5 +41,18 @@ public class MainVerticle extends AbstractVerticle {
                         res.cause().printStackTrace();
                     }
                 });
+
+        AtomicLong messageCounter = new AtomicLong(0);
+        AtomicLong lastStatsTime = new AtomicLong(System.currentTimeMillis());
+        vertx.eventBus().localConsumer("client.paint", msg -> {
+            messageCounter.incrementAndGet();
+            long messageTime = System.currentTimeMillis();
+            long timeDelta = messageTime - lastStatsTime.get();
+            if (timeDelta > 60_000) {
+                System.out.println("Messages per minute: " + (messageCounter.get() / (timeDelta / 60_000)));
+                messageCounter.set(0);
+                lastStatsTime.set(messageTime);
+            }
+        });
     }
 }
